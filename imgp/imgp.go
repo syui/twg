7517cce23d5f157dc07519d35b0b701e0bb8759d
syui <syui@syui.cf>
2023-01-14 13:21:35 +0900

    fix imgp

--- imgp/imgp.go
+++ imgp/imgp.go
@@ -11,7 +11,7 @@ import (
 
 func ImgPostp(c *cli.Context) error {
 	if c.NArg() == 0 {
-		fmt.Println("twg i ~/file/img.jpg $message")
+		fmt.Println("twg ii ~/file/img.jpg $tw_id")
 		os.Exit(0)
 	}
 
@@ -26,12 +26,13 @@ func ImgPostp(c *cli.Context) error {
 
 	api := oauth.GetOAuthApi()
 	id := c.Args().Get(1)
+	mes := c.Args().Get(2)
 	media, _ := api.UploadMedia(base64String)
 	v := url.Values{}
 	v.Add("in_reply_to_status_id", id)
 	v.Add("media_ids", media.MediaIDString)
 	v.Set("tweet_mode", "extended")
-	tweet, err := api.PostTweet(id, v)
+	tweet, err := api.PostTweet(mes,v)
 	if err != nil {
 		panic(err)
 	}
